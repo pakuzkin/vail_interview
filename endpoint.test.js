0f90fd72cb19c9871e1146d5d5c53dca8a5e81fa
Pasha <18251378+pakuzkin@users.noreply.github.com>
2023-03-22 10:56:23 -0700

    Test ping response body

--- endpoint.test.js
+++ endpoint.test.js
@@ -6,10 +6,19 @@ chai.use(chaiHttp);
 
 describe('Endpoint tests', () => {
     it('should return status 200', (done) => {
+        const msg = "hello"
         chai.request(app)
             .get('/api/ping')
+            .query({
+                "message": msg
+            })
             .end((err, res) => {
+                console.log(res.body)
                 chai.expect(res).to.have.status(200);
+                const body = res.body
+                chai.assert(body.echo == msg, `Wrong echo message returned`)
+                chai.assert(body.version == "1.0.0", `Wrong version returned`)
+                chai.assert(body.env == "development", `Wrong env returned`)
                 done();
             });
     });
