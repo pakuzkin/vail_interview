5115c72f58adb88b25008f4908061054b4428cd9
Pasha <18251378+pakuzkin@users.noreply.github.com>
2023-03-22 09:52:45 -0700

    Add ping endpoint metadata params

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const PORT = 30000
 
 app.get('/api/ping', (req, res) => {
     const msg = req.query.message
+    const env = process.env.NODE_ENV || 'development';
+    const version = process.env.APP_VERSION || 'unknown';
     const response = {
         "echo": msg,
-        "timestamp": "TODO",
-        "env": "TODO",
-        "version": "TODO"
+        "timestamp": Date.now(),
+        "env": env,
+        "version": version,
     }
     res.send(response)
 })
