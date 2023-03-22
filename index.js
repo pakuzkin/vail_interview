b39f55f5be5264ce87483e86690b25c53bcad983
Pasha <18251378+pakuzkin@users.noreply.github.com>
2023-03-22 09:44:43 -0700

    Add echo parameter to ping endpoint

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const app = express()
 const PORT = 30000
 
 app.get('/api/ping', (req, res) => {
-    res.send('pong!')
+    const msg = req.query.message
+    const response = {
+        "echo": msg,
+        "timestamp": "TODO",
+        "env": "TODO",
+        "version": "TODO"
+    }
+    res.send(response)
 })
 
 app.listen(PORT, () => {
